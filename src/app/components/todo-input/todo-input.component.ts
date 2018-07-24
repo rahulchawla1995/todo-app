a259eecacd865a59c5db199282829bdca94a50aa
rahulchawla1995 <rahulchawla.chawla@gmail.com>
2018-07-25 00:25:08 +0530

    dfre

---
+++ src/app/components/todo-input/todo-input.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.css']
+})
+export class TodoInputComponent implements OnInit {
+
+  public todoText: string;
+  public todoDate: string;
+
+  constructor(private todoService: TodoService) {
+    this.todoText = '';
+    this.todoDate='';
+  }
+
+  ngOnInit() {
+  }
+
+  private addTodo(): void {
+    this.todoService.addTodo(this.todoText+" "+this.todoDate);
+    this.todoText = '';
+  }
+}
